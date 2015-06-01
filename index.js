4b91f20143c6dc95a76e9fb7fdd2919008343da4
Takehiro YOSHIHAMA <take000x@gmail.com>
2015-06-01 20:22:53 +0900

    AccessToken を取得する動作を追加

--- index.js
+++ index.js
@@ -4,6 +4,9 @@
 
 var fs = require('fs');
 var querystring = require('querystring');
+var readline = require('readline-sync');
+var request = require('request');
+var open = require('open');
 
 if (process.argv[2] === undefined) {
 	console.log("argv:", process.argv);
@@ -26,5 +29,31 @@ if (process.argv[2] == "init") {
 	};
 
 	var url = config.auth_uri + "?" + querystring.encode(params);
-	console.log(url);
+	open(url);
+}
+
+if (process.argv[2] == "token") {
+	var config = JSON.parse(fs.readFileSync(",/config.json")).installed;
+	var code = readline.question("Input returned code:");
+	var params = {
+		grant_type: 'authorization_code',
+		code: code,
+		client_id: config.client_id,
+		client_secret: config.client_secret,
+		redirect_uri: config.redirect_uris[0]
+	};
+	var options = {
+		uri: config.token_uri,
+		form: params,
+		json: true
+	};
+	request.post(options, function (error, response, body) {
+		if (response.statusCode !== 200) {
+			console.log("Error:", error);
+			console.log("Status_body:", response.statusCode);
+			console.log("body:", body);
+			return false;
+		}
+		fs.writeFileSync(',/token.json', JSON.stringify(body));
+	});
 }
